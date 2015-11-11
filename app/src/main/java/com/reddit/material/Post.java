7544add6a14dc09ae3f05dd6b18c74beb9d8483a
Rushil Perera <pererarushil1940@gmail.com>
2015-11-11 10:32:34 -0500

    Removed unnecessary fields

--- app/src/main/java/com/reddit/material/Post.java
+++ app/src/main/java/com/reddit/material/Post.java
@@ -10,15 +10,12 @@ public class Post implements Serializable {
     private String domain;
     private String bannedBy;
     private String subreddit;
-    private String selfTextHTML;
-    private String selfText;
     private String suggestedSort;
     private String userReports;
     private String linkFlairText;
     private String id;
     //    private String fromKind;
     private int gilded;
-    private boolean archived;
     private boolean clicked;
     //    private String reportReasons;
     private String author;
@@ -33,20 +30,15 @@ public class Post implements Serializable {
     private String subredditID;
     private boolean hideScore;
     private double edited;
-    private int downs;
-    private String secureMediaEmbed;
     private boolean saved;
     //    private String removalReason;
-    private String postHint;
     private boolean stickied;
     private String from;
-    private boolean isSelf;
     private String fromID;
     private String permalink;
     private boolean locked;
     private String name;
     private String url;
-    private String authorFlairText;
     private boolean quarantine;
     private String title;
     private long createdUTC;
@@ -55,25 +47,20 @@ public class Post implements Serializable {
     private boolean visited;
 //    private String numReports;
 
-    public Post(String domain, String bannedBy, String subreddit, String selfTextHTML, String
-            selfText, String suggestedSort, String userReports, String linkFlairText, String id, int gilded, boolean
-                        archived, boolean clicked, String author, String media, int score, boolean over18, boolean
-            hidden, String previewImageURL, int numComments, String thumbnailURL, String subredditID, boolean
-            hideScore, boolean edited, int downs, String secureMediaEmbed, boolean saved, String postHint, boolean
-            stickied, String from, boolean isSelf, String fromID, String permalink, boolean locked, String name,
-                String url, String authorFlairText, boolean quarantine, String title, long createdUTC, String
-                        distinguished, String modReports, boolean visited) {
+    public Post(String domain, String bannedBy, String subreddit, String suggestedSort, String userReports, String
+            linkFlairText, String id, int gilded, boolean clicked, String author, String media, int score, boolean
+                        over18, boolean hidden, String previewImageURL, int numComments, String thumbnailURL, String
+                        subredditID, boolean hideScore, boolean saved, boolean stickied, String from, String fromID,
+                String permalink, boolean locked, String name, String url, boolean quarantine, String title, long
+                        createdUTC, String distinguished, String modReports, boolean visited) {
         this.domain = domain;
         this.bannedBy = bannedBy;
         this.subreddit = subreddit;
-        this.selfTextHTML = selfTextHTML;
-        this.selfText = selfText;
         this.suggestedSort = suggestedSort;
         this.userReports = userReports;
         this.linkFlairText = linkFlairText;
         this.id = id;
         this.gilded = gilded;
-        this.archived = archived;
         this.clicked = clicked;
         this.author = author;
         this.media = media;
@@ -86,19 +73,14 @@ public class Post implements Serializable {
         this.subredditID = subredditID;
         this.hideScore = hideScore;
         this.edited = 0.0;
-        this.downs = downs;
-        this.secureMediaEmbed = secureMediaEmbed;
         this.saved = saved;
-        this.postHint = postHint;
         this.stickied = stickied;
         this.from = from;
-        this.isSelf = isSelf;
         this.fromID = fromID;
         this.permalink = permalink;
         this.locked = locked;
         this.name = name;
         this.url = url;
-        this.authorFlairText = authorFlairText;
         this.quarantine = quarantine;
         this.title = title;
         this.createdUTC = createdUTC;
@@ -107,25 +89,21 @@ public class Post implements Serializable {
         this.visited = visited;
     }
 
-    public Post(String domain, String banned_by, String subreddit, String selfTextHTML, String selftext, String
-            suggestedSort, String user_reports, String linkFlairText, String id, int gilded, boolean archived,
-                boolean clicked, String author, String media, int score, boolean over_18, boolean hidden, String
-                        previewImageURL, int num_comments, String thumbnail, String subreddit_id, boolean hide_score,
-                double edited, int downs, String secureMediaEmbed, boolean saved, String postHint, boolean stickied,
-                String from, boolean is_self, String fromID, String permalink, boolean locked, String name, String
-                        url, String authorFlairText, boolean quarantine, String title, long created_utc, String
-                        distinguished, String mod_reports, boolean visited) {
+    public Post(String domain, String banned_by, String subreddit, String
+            suggestedSort, String user_reports, String linkFlairText, String id, int gilded, boolean clicked, String
+                        author, String media, int score, boolean over_18, boolean hidden, String previewImageURL, int
+                        num_comments, String thumbnail, String subreddit_id, boolean hide_score, double edited,
+                boolean saved, boolean stickied, String from, String fromID, String permalink, boolean locked, String
+                        name, String url, boolean quarantine, String title, long created_utc, String distinguished,
+                String mod_reports, boolean visited) {
         this.domain = domain;
         bannedBy = banned_by;
         this.subreddit = subreddit;
-        this.selfTextHTML = selfTextHTML;
-        selfText = selftext;
         this.suggestedSort = suggestedSort;
         userReports = user_reports;
         this.linkFlairText = linkFlairText;
         this.id = id;
         this.gilded = gilded;
-        this.archived = archived;
         this.clicked = clicked;
         this.author = author;
         this.media = media;
@@ -138,19 +116,14 @@ public class Post implements Serializable {
         subredditID = subreddit_id;
         hideScore = hide_score;
         this.edited = edited;
-        this.downs = downs;
-        this.secureMediaEmbed = secureMediaEmbed;
         this.saved = saved;
-        this.postHint = postHint;
         this.stickied = stickied;
         this.from = from;
-        isSelf = is_self;
         this.fromID = fromID;
         this.permalink = permalink;
         this.locked = locked;
         this.name = name;
         this.url = url;
-        this.authorFlairText = authorFlairText;
         this.quarantine = quarantine;
         this.title = title;
         createdUTC = created_utc;
@@ -171,14 +144,6 @@ public class Post implements Serializable {
         return subreddit;
     }
 
-    public String getSelfTextHTML() {
-        return selfTextHTML;
-    }
-
-    public String getSelfText() {
-        return selfText;
-    }
-
     public String getSuggestedSort() {
         return suggestedSort;
     }
@@ -197,10 +162,6 @@ public class Post implements Serializable {
 
     public int getGilded() {
         return gilded;
-    }
-
-    public boolean isArchived() {
-        return archived;
     }
 
     public boolean isClicked() {
@@ -251,20 +212,8 @@ public class Post implements Serializable {
         return edited;
     }
 
-    public int getDowns() {
-        return downs;
-    }
-
-    public String getSecureMediaEmbed() {
-        return secureMediaEmbed;
-    }
-
     public boolean isSaved() {
         return saved;
-    }
-
-    public String getPostHint() {
-        return postHint;
     }
 
     public boolean isStickied() {
@@ -273,10 +222,6 @@ public class Post implements Serializable {
 
     public String getFrom() {
         return from;
-    }
-
-    public boolean isSelf() {
-        return isSelf;
     }
 
     public String getFromID() {
@@ -297,10 +242,6 @@ public class Post implements Serializable {
 
     public String getURL() {
         return url;
-    }
-
-    public String getAuthorFlairText() {
-        return authorFlairText;
     }
 
     public boolean isQuarantine() {
