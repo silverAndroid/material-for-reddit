58d32b57dd2fcb9ba194c2e4ecd732eb573868cf
Rushil Perera <pererarushil1940@gmail.com>
2015-11-22 22:00:30 -0500

    Can send and format comments, load GIFs, YouTube videos and webpages

--- app/src/main/java/com/reddit/material/PostViewHolder.java
+++ app/src/main/java/com/reddit/material/PostViewHolder.java
@@ -1,62 +1,313 @@
 package com.reddit.material;
 
-import android.content.Context;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.customtabs.CustomTabsIntent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ImageView;
+import android.webkit.URLUtil;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.Serializable;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.reddit.material.libraries.google.CustomTabActivityHelper;
 
 /**
  * Created by Rushil Perera on 10/28/2015.
  */
-public class PostViewHolder extends RecyclerView.ViewHolder {
+public class PostViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     final TextView title;
-    final ImageView image;
     final TextView lineOneInfo;
     final TextView source;
     final TextView timeSubredditInfo;
     final TextView gilded;
     final TextView flair;
     final TextView nsfwTag;
+    final TextView selfText;
+    final SimpleDraweeView image;
     final ProgressBar loading;
-    private final Context context;
     final CardView card;
+    final ImageButton upvote;
+    final ImageButton downvote;
+    final ImageButton reply;
+    final ImageButton formatBold;
+    final ImageButton formatItalics;
+    final ImageButton formatStrikethrough;
+    final ImageButton formatSuperscript;
+    final ImageButton formatLink;
+    final ImageButton formatQuote;
+    final ImageButton formatCodeTags;
+    final ImageButton formatBulletList;
+    final ImageButton formatNumberList;
+    final Button cancel;
+    final Button submit;
+    final EditText editMessage;
+    final LinearLayout sendLayout;
+    private final Activity activity;
 
-    public PostViewHolder(final View itemView, final Context context) {
+    public PostViewHolder(final View itemView, final Activity activity) {
         super(itemView);
-        this.context = context;
+        this.activity = activity;
         title = (TextView) itemView.findViewById(R.id.title);
-        image = (ImageView) itemView.findViewById(R.id.image);
+        image = (SimpleDraweeView) itemView.findViewById(R.id.image);
         lineOneInfo = (TextView) itemView.findViewById(R.id.lineOneInfo);
         source = (TextView) itemView.findViewById(R.id.source);
         timeSubredditInfo = (TextView) itemView.findViewById(R.id.timeSubredditInfo);
-        gilded = (TextView) itemView.findViewById(R.id.gold_count);
+        gilded = (TextView) itemView.findViewById(R.id.gild_count);
         flair = (TextView) itemView.findViewById(R.id.flair);
         nsfwTag = (TextView) itemView.findViewById(R.id.nsfw_tag);
         loading = (ProgressBar) itemView.findViewById(R.id.loading);
         card = (CardView) itemView.findViewById(R.id.card);
+        upvote = (ImageButton) itemView.findViewById(R.id.btn_upvote);
+        downvote = (ImageButton) itemView.findViewById(R.id.btn_downvote);
+        selfText = (TextView) itemView.findViewById(R.id.self_text);
+
+        LinearLayout linearLayout = (LinearLayout) itemView.findViewById(R.id.linear_layout);
+        sendLayout = (LinearLayout) linearLayout.findViewById(R.id.include_send);
+        sendLayout.setVisibility(View.GONE);
+        reply = (ImageButton) linearLayout.findViewById(R.id.btn_reply);
+        reply.setOnClickListener(this);
+        editMessage = (EditText) linearLayout.findViewById(R.id.edit_message);
+
+        cancel = (Button) linearLayout.findViewById(R.id.btn_cancel);
+        cancel.setOnClickListener(this);
+        submit = (Button) linearLayout.findViewById(R.id.btn_submit);
+
+        formatBold = (ImageButton) sendLayout.findViewById(R.id.btn_bold);
+        formatBold.setOnClickListener(this);
+        formatItalics = (ImageButton) sendLayout.findViewById(R.id.btn_italics);
+        formatItalics.setOnClickListener(this);
+        formatStrikethrough = (ImageButton) sendLayout.findViewById(R.id.btn_strikethrough);
+        formatStrikethrough.setOnClickListener(this);
+        formatSuperscript = (ImageButton) sendLayout.findViewById(R.id.btn_superscript);
+        formatSuperscript.setOnClickListener(this);
+        formatLink = (ImageButton) sendLayout.findViewById(R.id.btn_link);
+        formatLink.setOnClickListener(this);
+        formatQuote = (ImageButton) sendLayout.findViewById(R.id.btn_quote);
+        formatQuote.setOnClickListener(this);
+        formatCodeTags = (ImageButton) sendLayout.findViewById(R.id.btn_code);
+        formatCodeTags.setOnClickListener(this);
+        formatBulletList = (ImageButton) sendLayout.findViewById(R.id.btn_bullet_list);
+        formatBulletList.setOnClickListener(this);
+        formatNumberList = (ImageButton) sendLayout.findViewById(R.id.btn_number_list);
+        formatNumberList.setOnClickListener(this);
     }
 
-    public void setImageURL(final String url) {
+    public void setURL(final String url) {
         image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (ConstantMap.getInstance().isGIF(url)) {
-                    Intent intent = new Intent(context, VideoActivity.class);
+                if (ConstantMap.getInstance().isYoutube(url)) {
+                    Intent intent = new Intent(activity, YouTubeActivity.class);
                     intent.putExtra("url", url);
-                    context.startActivity(intent);
+                    activity.startActivity(intent);
+                } else if (ConstantMap.getInstance().isGIF(url)) {
+                    Intent intent = new Intent(activity, VideoActivity.class);
+                    intent.putExtra("url", url);
+                    activity.startActivity(intent);
                 } else if (ConstantMap.getInstance().isImage(url)) {
-                    Intent intent = new Intent(context, ImageActivity.class);
+                    Intent intent = new Intent(activity, ImageActivity.class);
                     intent.putExtra("url", url);
-                    context.startActivity(intent);
+                    activity.startActivity(intent);
+                } else {
+                    CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        builder.setToolbarColor(activity.getResources().getColor(R.color.colorPrimary, null));
+                    } else {
+                        builder.setToolbarColor(activity.getResources().getColor(R.color.colorPrimary));
+                    }
+                    builder.setShowTitle(true);
+                    CustomTabActivityHelper.openCustomTab(activity, builder.build(), Uri.parse(url), null);
                 }
             }
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_cancel:
+                sendLayout.setVisibility(View.GONE);
+                editMessage.getText().clear();
+                break;
+            case R.id.btn_reply:
+                if (!Authentication.getInstance().isLoggedIn())
+                    Toast.makeText(activity, "You must be logged in to comment!", Toast.LENGTH_SHORT).show();
+                else
+                    sendLayout.setVisibility(View.VISIBLE);
+                break;
+            case R.id.btn_bold:
+                int selectionStart = editMessage.getSelectionStart();
+                int selectionEnd = editMessage.getSelectionEnd();
+                if (selectionStart != -1 || selectionEnd == -1) {
+                    String originalMessage = editMessage.getText().toString();
+                    String newMessage = originalMessage.substring(0, selectionStart) + "**" + originalMessage
+                            .substring(selectionStart, selectionEnd).replaceAll("(\\*\\*)", "") + "**" +
+                            originalMessage.substring(selectionEnd, originalMessage.length());
+                    editMessage.setText(newMessage);
+                } else
+                    editMessage.append(" ****");
+                break;
+            case R.id.btn_italics:
+                selectionStart = editMessage.getSelectionStart();
+                selectionEnd = editMessage.getSelectionEnd();
+                if (selectionStart != -1 || selectionEnd == -1) {
+                    String originalMessage = editMessage.getText().toString();
+                    String newMessage = originalMessage.substring(0, selectionStart) + "*" + originalMessage
+                            .substring(selectionStart, selectionEnd).replaceAll("(?<![*])[*](?![*])", "") + "*" +
+                            originalMessage.substring(selectionEnd, originalMessage.length());
+                    editMessage.setText(newMessage);
+                } else
+                    editMessage.append(" **");
+                break;
+            case R.id.btn_strikethrough:
+                selectionStart = editMessage.getSelectionStart();
+                selectionEnd = editMessage.getSelectionEnd();
+                if (selectionStart != -1 || selectionEnd == -1) {
+                    String originalMessage = editMessage.getText().toString();
+                    String newMessage = originalMessage.substring(0, selectionStart) + "~~" + originalMessage
+                            .substring(selectionStart, selectionEnd).replaceAll("(~~)", "") + "~~" +
+                            originalMessage.substring(selectionEnd, originalMessage.length());
+                    editMessage.setText(newMessage);
+                } else
+                    editMessage.append(" ~~");
+                break;
+            case R.id.btn_superscript:
+                selectionStart = editMessage.getSelectionStart();
+                selectionEnd = editMessage.getSelectionEnd();
+                if (selectionStart != -1 || selectionEnd == -1) {
+                    String originalMessage = editMessage.getText().toString();
+                    String newMessage = originalMessage.substring(0, selectionStart) + "^" + originalMessage
+                            .substring(selectionStart, selectionEnd).replace("^", "") + originalMessage.substring
+                            (selectionEnd, originalMessage.length());
+                    editMessage.setText(newMessage);
+                } else
+                    editMessage.append("^");
+                break;
+            case R.id.btn_link:
+                final int selectionStartURL = editMessage.getSelectionStart();
+                final int selectionEndURL = editMessage.getSelectionEnd();
+                final AlertDialog alertDialog = new AlertDialog.Builder(activity)
+                        .setView(R.layout.layout_url_dialog)
+                        .setTitle("Enter URL")
+                        .setPositiveButton("OK", null)
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+                alertDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+                    @Override
+                    public void onShow(final DialogInterface dialogInterface) {
+                        Button ok = alertDialog.getButton(DialogInterface.BUTTON_POSITIVE);
+                        ok.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Dialog dialog = (Dialog) dialogInterface;
+                                EditText urlEdit = (EditText) dialog.findViewById(R.id.url);
+                                String url = urlEdit.getText().toString();
+                                if (URLUtil.isValidUrl(url)) {
+                                    if (selectionStartURL != -1 || selectionEndURL == -1) {
+                                        String originalMessage = editMessage.getText().toString();
+                                        String newMessage = originalMessage.substring(0, selectionStartURL) + "[" +
+                                                originalMessage.substring(selectionStartURL, selectionEndURL) + "]("
+                                                + url + ")" + originalMessage.substring(selectionEndURL,
+                                                originalMessage.length());
+                                        editMessage.setText(newMessage);
+                                    } else
+                                        editMessage.append("[](" + url + ")");
+                                    dialog.dismiss();
+                                } else
+                                    urlEdit.setError(activity.getResources().getString(R.string.error_invalid_url));
+                            }
+                        });
+                    }
+                });
+                alertDialog.show();
+                break;
+            case R.id.btn_quote:
+                selectionStart = editMessage.getSelectionStart();
+                selectionEnd = editMessage.getSelectionEnd();
+                //TODO: Adding the > to the beginning of the line
+                if (selectionStart != -1 || selectionEnd == -1) {
+                    String originalMessage = editMessage.getText().toString();
+                    String newMessage = originalMessage.substring(0, selectionStart) + "\n> " + originalMessage
+                            .substring(selectionStart, selectionEnd) + originalMessage.substring(selectionEnd,
+                            originalMessage.length());
+                    editMessage.setText(newMessage);
+                } else {
+                    if (editMessage.length() > 0)
+                        editMessage.append("\n> ");
+                    else
+                        editMessage.setText(">");
+                }
+                break;
+            case R.id.btn_code:
+                selectionStart = editMessage.getSelectionStart();
+                selectionEnd = editMessage.getSelectionEnd();
+                //TODO: Adding 4 spaces to the beginning of the line
+                if (selectionStart != -1 || selectionEnd == -1) {
+                    String originalMessage = editMessage.getText().toString();
+                    String newMessage = originalMessage.substring(0, selectionStart) + "\n    " + originalMessage
+                            .substring(selectionStart, selectionEnd) + originalMessage.substring(selectionEnd,
+                            originalMessage.length());
+                    editMessage.setText(newMessage);
+                } else {
+                    if (editMessage.length() > 0)
+                        editMessage.append("\n    ");
+                    else
+                        editMessage.setText("    ");
+                }
+                break;
+            case R.id.btn_bullet_list:
+                selectionStart = editMessage.getSelectionStart();
+                selectionEnd = editMessage.getSelectionEnd();
+                //TODO: Adding bullet to the beginning of the line
+                if (selectionStart != -1 || selectionEnd == -1) {
+                    String originalMessage = editMessage.getText().toString();
+                    String newMessage = originalMessage.substring(0, selectionStart) + "\n*" + originalMessage
+                            .substring(selectionStart, selectionEnd) + originalMessage.substring(selectionEnd,
+                            originalMessage.length());
+                    editMessage.setText(newMessage);
+                } else {
+                    if (editMessage.length() > 0)
+                        editMessage.append("\n*");
+                    else
+                        editMessage.setText("*");
+                }
+                break;
+            case R.id.btn_number_list:
+                selectionStart = editMessage.getSelectionStart();
+                selectionEnd = editMessage.getSelectionEnd();
+                //TODO: Adding numbering to the beginning of the line
+                if (selectionStart != -1 || selectionEnd == -1) {
+                    String originalMessage = editMessage.getText().toString();
+                    String newMessage = originalMessage.substring(0, selectionStart) + "\n1. " + originalMessage
+                            .substring(selectionStart, selectionEnd) + originalMessage.substring(selectionEnd,
+                            originalMessage.length());
+                    editMessage.setText(newMessage);
+                } else {
+                    if (editMessage.length() > 0)
+                        editMessage.append("\n1. ");
+                    else
+                        editMessage.setText("1. ");
+                }
+            default:
+                break;
+        }
     }
 }
