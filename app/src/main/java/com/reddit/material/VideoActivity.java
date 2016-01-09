471aade60b265848bb84a6e95461b234e3a15560
Rushil Perera <pererarushil1940@gmail.com>
2016-01-09 13:12:39 -0500

    Removed overriden onDestroy method, added MediaController to VideoView, changed variable name to view

--- app/src/main/java/com/reddit/material/VideoActivity.java
+++ app/src/main/java/com/reddit/material/VideoActivity.java
@@ -1,12 +1,10 @@
 package com.reddit.material;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.MediaController;
 import android.widget.ProgressBar;
 import android.widget.VideoView;
 
@@ -15,7 +13,6 @@ import com.koushikdutta.ion.Ion;
 public class VideoActivity extends AppCompatActivity {
 
     private final String ARG_URL = "video_url";
-    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +22,14 @@ public class VideoActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         String url = getIntent().getStringExtra("url");
-        videoView = (VideoView) findViewById(R.id.video);
+        VideoView view = (VideoView) findViewById(R.id.video);
+        view.setMediaController(new MediaController(VideoActivity.this, true));
         ProgressBar loading = (ProgressBar) findViewById(R.id.loading);
         if (ConstantMap.getInstance().isGIF(url))
-            ConnectionSingleton.getInstance().loadGIF(url, videoView, loading, ARG_URL);
+            ConnectionSingleton.getInstance().loadGIF(url, view, loading, ARG_URL);
         else
-            ConnectionSingleton.getInstance().loadVideo(url, videoView, loading);
+            ConnectionSingleton.getInstance().loadVideo(url, view, loading);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
     }
@@ -52,12 +42,5 @@ public class VideoActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        videoView.stopPlayback();
-        Ion.getDefault(getBaseContext()).cancelAll(ARG_URL);
     }
 }
