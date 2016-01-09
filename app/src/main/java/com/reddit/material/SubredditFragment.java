45efb8fe2f11228e04161b2113674d2017279b79
Rushil Perera <pererarushil1940@gmail.com>
2016-01-09 13:11:28 -0500

    Added sort and time parameters to Fragment

--- app/src/main/java/com/reddit/material/SubredditFragment.java
+++ app/src/main/java/com/reddit/material/SubredditFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,24 +14,33 @@ import android.widget.ProgressBar;
 
 public class SubredditFragment extends Fragment {
 
+    private static final String ARG_R = "r/";
+    private static final String ARG_SORT = "sort";
+    private static final String ARG_TIME = "time";
+
     private static SubredditFragment instance;
     private PostAdapter adapter;
     private SwipeRefreshLayout refresh;
     private ProgressBar loading;
 
     public static SubredditFragment newInstance() {
-        SubredditFragment fragment = new SubredditFragment();
-        Bundle bundle = new Bundle(1);
-        bundle.putString("r/", "");
-        fragment.setArguments(bundle);
-        instance = fragment;
-        return fragment;
+        return newInstance("frontpage");
     }
 
     public static SubredditFragment newInstance(String subreddit) {
+        return newInstance(subreddit, "hot", "");
+    }
+
+    public static SubredditFragment newInstance(String subreddit, String sort) {
+        return newInstance(subreddit, sort, "");
+    }
+
+    public static SubredditFragment newInstance(String subreddit, String sort, String time) {
         SubredditFragment fragment = new SubredditFragment();
         Bundle bundle = new Bundle(1);
-        bundle.putString("r/", subreddit.equalsIgnoreCase("frontpage") ? "" : subreddit);
+        bundle.putString(ARG_R, subreddit.equalsIgnoreCase("frontpage") ? "" : subreddit);
+        bundle.putString(ARG_SORT, sort.equalsIgnoreCase("hot") ? "" : sort);
+        bundle.putString(ARG_TIME, time);
         fragment.setArguments(bundle);
         instance = fragment;
         return fragment;
@@ -67,13 +77,15 @@ public class SubredditFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         adapter = new PostAdapter(getActivity());
         recyclerView.setAdapter(adapter);
-        ConnectionSingleton.getInstance().getSubredditData(getArguments().getString("r/"));
+        ConnectionSingleton.getInstance().getSubredditData(getArguments().getString(ARG_R), getArguments().getString
+                (ARG_SORT), getArguments().getString(ARG_TIME));
         return view;
     }
 
     public void refresh() {
         adapter.clearPosts();
-        ConnectionSingleton.getInstance().getSubredditData(getArguments().getString("r/"));
+        ConnectionSingleton.getInstance().getSubredditData(getArguments().getString(ARG_R), getArguments().getString
+                (ARG_SORT), getArguments().getString(ARG_TIME));
     }
 
     public PostAdapter getAdapter() {
